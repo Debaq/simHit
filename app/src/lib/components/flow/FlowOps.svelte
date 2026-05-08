4ad56f5c92740c9de7b977a7c3c327446dc73af2
Nicolás Baier Quezada <15371725+Debaq@users.noreply.github.com>
2026-05-08 09:56:23 -0400

    chore: borrar componentes flow y logos firmware sin uso

--- app/src/lib/components/flow/FlowOps.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import { useSvelteFlow } from '@xyflow/svelte';
-  let { onReady }: { onReady: (sf: ReturnType<typeof useSvelteFlow>) => void } = $props();
-  const sf = useSvelteFlow();
-  $effect(() => onReady(sf));
-</script>
